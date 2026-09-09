7711fc0aaa28a05ff242a1855a2cb65f22671864
Kihira <d.kertesz16@gmail.com>
2026-09-09 20:18:34 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Racecar
+Transmog your pet into the burrowed form of thd doom of mokhaitol
--- src/main/java/com/racecar/Racecar.java
+++ src/main/java/com/racecar/Racecar.java
@@ -1,13 +1,11 @@
-package com.example;
+package com.racecar;
 
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -16,13 +14,11 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class Racecar extends Plugin
 {
 	@Inject
 	private Client client;
 
-	@Inject
-	private ExampleConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -41,13 +37,7 @@ public class ExamplePlugin extends Plugin
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says ",null);
 		}
-	}
-
-	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(ExampleConfig.class);
 	}
 }
